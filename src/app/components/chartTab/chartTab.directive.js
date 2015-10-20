d4184a1ffe4832800bc2975b24361795baed15b7
ryysd <ry.ysd01@gmail.com>
2015-10-20 21:15:21 +0900

    [add] 表示中のタブの色を変更する機能を追加

--- src/app/components/chartTab/chartTab.directive.js
+++ src/app/components/chartTab/chartTab.directive.js
@@ -9,17 +9,38 @@ export function ChartTabDirective() {
     },
     controller: ChartTabController,
     controllerAs: 'chartTab',
-    bindToController: true
+    bindToController: true,
+    link: postLink
   };
+
+  function postLink(scope, element, attrs, ctrl) {
+    element.bind('scroll', function() {
+      scope.$apply(function() {
+        ctrl.setActiveTab((getActiveTarget(element) || {}).id);
+      });
+    });
+  }
+
+  function getActiveTarget(element) {
+    var raw = element[0];
+
+    return angular.element('chart').filter(function(idx, chart) {
+      return chart.offsetTop >= raw.scrollTop;
+    }).sort(function(a, b) {b.offsetTop - a.offsetTop})[0];
+  }
 
   return directive;
 }
 
 class ChartTabController {
-  constructor ($scope, $log) {
+  constructor ($scope, $log, $location, $anchorScroll) {
     'ngInject';
 
     this.$log = $log;
+    this.$location = $location;
+    this.$anchorScroll = $anchorScroll;
+    this.activeTab = angular.element('chart')[0].id;
+
     this.activate();
   }
 
@@ -27,6 +48,17 @@ class ChartTabController {
       this.$log.info('Activated ChartTab View');
   }
 
-  getChartData() {
+  scrollTo(id) {
+    this.$location.hash(id);
+    this.$anchorScroll();
+  }
+
+  setActiveTab(id) {
+    this.activeTab = id;
+  }
+
+  getClass(id) {
+    console.log(this.activeTab);
+    return this.activeTab == id ? 'chart-tab-active' : '';
   }
 }
