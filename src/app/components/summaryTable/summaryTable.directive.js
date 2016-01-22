ef8fc64446512c11fbf42f9eb7b56319499518bb
ysm001 <ry.ysd01@gmail.com>
2016-01-22 15:48:09 +0900

    [add] ログの削除機能を実装

--- src/app/components/summaryTable/summaryTable.directive.js
+++ src/app/components/summaryTable/summaryTable.directive.js
@@ -15,7 +15,7 @@ export function SummaryTableDirective() {
 
   function postLink(scope, element, attrs, controller) {
     scope.$watch(() => {
-      return controller.isActivated;
+      return controller.isUpdated();
     }, (newValue) => {
       if (newValue) {
         const table = element.find('table');
@@ -23,6 +23,7 @@ export function SummaryTableDirective() {
         table.find('th')[0].remove();
         table.removeAttr('data-upgraded');
         componentHandler.upgradeDom();
+        controller.setUpdated(false);
       }
     });
   }
@@ -31,15 +32,19 @@ export function SummaryTableDirective() {
 }
 
 class SummaryTableController {
-  constructor ($scope, $log, verification, appStatus) {
+  constructor ($scope, $log, $timeout, verification, appStatus, ModalService) {
     'ngInject';
 
     this.$scope = $scope;
     this.$log = $log;
+    this.$timeout = $timeout;
     this.summaries = [];
     this.appStatus = appStatus;
     this.verification = verification;
     this.isActivated = false;
+    this.ModalService = ModalService;
+    this.selectedRecords = [];
+    this.messageType = '';
 
     this.watchUpdateFlag();
     this.activate();
@@ -47,7 +52,6 @@ class SummaryTableController {
 
   activate() {
     return this.fetchAndUpdate().then((summaries) => {
-      this.isActivated = true;
       this.$log.info('Activated Summaries View');
     });
   }
@@ -68,6 +72,10 @@ class SummaryTableController {
       if (summaries.length > 0) {
         this.appStatus.currentId = summaries[0]._id;
       }
+
+      this.$timeout(() => {
+        this.updated = true;
+      }, 0);
     });
   }
 
@@ -87,8 +95,117 @@ class SummaryTableController {
     this.appStatus.currentId = element._id;
   }
 
+  onRecordClicked() {
+    this.$timeout(() => {
+      const selectedElements = angular.element('.summary-table tbody .is-selected');
+      this.selectedRecords = selectedElements.toArray().map((element) => {
+        return {
+          id: element.getAttribute('id'),
+          name: element.getAttribute('name'),
+          createdAt: element.getAttribute('created-at')
+        }
+      });
+    }, 0);
+  }
+
   onRemoveButtonClicked() {
-    const targets = angular.element('.summary-table tbody .is-checked');
-    console.log(targets.length);
+    console.log(this.selectedRecords);
+    this.ModalService.showModal({
+      templateUrl: 'app/components/summaryTable/removeDialog/removeDialog.template.html',
+      controller: RemoveDialogController,
+      controllerAs: 'removeDialog'
+    }).then((modal) => {
+      componentHandler.upgradeDom();
+      modal.controller.setTargets(this.selectedRecords);
+      modal.controller.show();
+    });
+  }
+
+  hasSelectedRecords() {
+    return this.selectedRecords.length != 0;
+  }
+
+  isUpdated() {
+    return this.updated;
+  }
+
+  setUpdated(updated) {
+    this.updated = updated;
+  }
+}
+
+class RemoveDialogController {
+  constructor ($scope, $log, $interval, $timeout, appStatus, verification, ModalService, close) {
+    'ngInject';
+
+    this.$log = $log;
+    this.$scope = $scope;
+    this.$interval = $interval;
+    this.$timeout = $timeout;
+    this.appStatus = appStatus;
+    this.verification = verification;
+    this.ModalService = ModalService;
+    this.modalClose = close;
+    this.hidden = false;
+    this.isRemoving = false;
+    this.isRemoved = false;
+
+    this.targets = null;
+  }
+
+  remove() {
+    this.isRemoving = true;
+
+    const promises = this.targets.map((target) => {
+      return this.verification.remove(target.id).then(() => {
+        this.$timeout(() => {
+          target.isRemoved = true;
+        }, 0);
+      });
+    });
+
+    Promise.all(promises).then(() => {
+      this.isRemoving = false;
+      this.message = 'Successfully deleted.';
+      this.messageType = 'success';
+      this.$timeout(() => {
+        this.isRemoved = true;
+        this.appStatus.summaryUpdated = true;
+      }, 0);
+    }).catch((error) => {
+      this.appStatus.summaryUpdated = true;
+      this.message = error;
+      this.messageType = 'error';
+      this.isRemoving = false;
+    });
+  }
+
+  hasErrorMessage() {
+    return this.messageType == 'error';
+  }
+
+  hasSuccessMessage() {
+    return this.messageType == 'success';
+  }
+
+  show() {
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
+
+  visible() {
+    return !this.hidden;
+  }
+
+  close(result) {
+    this.modalClose(result, 500);
+    this.hide();
+  }
+
+  setTargets(targets) {
+    this.targets = targets;
   }
 }
